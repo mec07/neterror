e960ab3a534bd6a4dff0934596855531c3df592e
Marc Coury <gloriphobia@gmail.com>
2021-05-10 17:57:19 +0100

    update comment in net_error.go file

--- net_error.go
+++ net_error.go
@@ -7,9 +7,9 @@ import (
 )
 
 // GetNetError recursively checks the chain of errors until it finds an error
-// from the net package that fulfills the net.Error interface. If we find one
-// that means its safe to say that a network error has occurred. See:
-// https://godoc.org/net#Error for more information on the net.Error interface.
+// that fulfills the net.Error interface. If we find one that means its safe to
+// say that a network error has occurred. See: https://godoc.org/net#Error for
+// more information on the net.Error interface.
 func GetNetError(err error) (net.Error, bool) {
 	for {
 		if err == nil {
